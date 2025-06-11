66cd22f8c6336248dc8d85388b21544ec817f62a
smartwayze <info.smartwayze@gmail.com>
2025-06-11 22:31:48 +0500

    Add files via upload

---
+++ android/app/practice dart file.dart
@@ -0,0 +1,15 @@
+//loops in dart
+main(){
+  int a=4,i;
+  for(i=0;i<5;i++) {
+    print('hello');
+  }
+while(a<10){
+    print('hello 2');
+    a++;
+}
+  do{
+    print('hello 3');
+    a--;
+}while(a>2);
+}
